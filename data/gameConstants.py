ca02d18c11b914b13de68466b0b29a76865bab26
ouolim <ouolim@eideo.cz>
2024-07-02 22:04:42 +0200

    Updated items and gameConstants on camp

--- data/gameConstants.py
+++ data/gameConstants.py
@@ -11,3 +11,23 @@ childrenCraftingCodes = {
 	'ondraCode123': 'Ondra',
 	'HelikCode123' : 'Heliodor'
 }
+
+roles = {
+	1: 'enginnerka',
+	2: 'pavouk',
+	3: 'kuchař',
+	4: 'wigfrid',
+	5: 'učitelka',
+	6: 'vodník',
+}
+
+childrenRoles = {
+	'kajacode123': 'Kája',
+	'pajacode123': 'Pája',
+	'strakaCode123': 'Jiří',
+	'kubaCode12345': 'Jakub',
+	'remziCode123': 'Remzí',
+	'risaCode123': 'Ríša',
+	'ondraCode123': 'Ondra',
+	'HelikCode123': 'Heliodor'
+}
--- data/items.py
+++ data/items.py
@@ -6,7 +6,7 @@ gameItems = {
 	4: 'pavučina',  # silk
 	5: 'klacíky',
 	6: 'pazourek',  # ostré kamínky
-	#7: 'prasečí kůže',
+	7: 'prasečí kůže',
 
 	# Uncommon
 	8: 'zlato',
@@ -18,11 +18,25 @@ gameItems = {
 
 	# Rare
 	14: 'vzácná ruda',  # na gemy
-	15: 'hračka z hrobu',
+	#15: 'hračka z hrobu',
 	#16: 'svítící perla z moře',
 	17: 'prkna',
 	18: 'truhla',
 	19: 'opracovaný kámen',
 	20: 'provaz',
-	21: 'oheň',
-	22: 'opracovaný rákos'}
+	21: 'malý ohýnek',
+	22: 'opracovaný rákos',
+	23: 'vědecká stanice',
+	24: 'velký oheň',
+	25: 'lednička',
+	26: 'stan',
+	27: 'sekyra',
+	28: 'bojová helma',
+	29: 'prasečí domeček',
+	30: 'batoh',
+	31: 'kopí',
+	32: 'dřevěná palisáda',
+	33: 'brýle',
+	34: 'hrnec',
+	35: 'katapult',
+	36: 'krumpáč'}
