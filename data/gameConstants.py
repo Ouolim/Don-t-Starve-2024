d07db00e55be1232cc4f51badcac07e417952ad8
ouolim <ouolim@eideo.cz>
2024-09-22 14:39:30 +0200

    Added children roles

--- data/gameConstants.py
+++ data/gameConstants.py
@@ -1,10 +1,13 @@
+# pregenerated codes that confirm/cancel crafting/show tooltip printed in handbook
 yesCode = "OKCrafting"
 noCode = "NOCrafting"
+tooltipCode = "showTooltip"
 
+# pregenerated codes for players, printed in handbook
 childrenCraftingCodes = {
 	'kajacode123' : 'Kája',
 	'pajacode123': 	'Pája',
-	'strakaCode123': 'Jiří',
+	'strakaCode123': 'Straka',
 	'kubaCode12345':'Jakub',
 	'remziCode123': 'Remzí',
 	'risaCode123' : 'Ríša',
@@ -12,6 +15,7 @@ childrenCraftingCodes = {
 	'HelikCode123' : 'Heliodor'
 }
 
+# role ids for crafting restrictions
 roles = {
 	1: 'enginnerka',
 	2: 'pavouk',
@@ -21,13 +25,14 @@ roles = {
 	6: 'vodník',
 }
 
+# who chose what role in scout camp
 childrenRoles = {
-	'kajacode123': 'Kája',
-	'pajacode123': 'Pája',
-	'strakaCode123': 'Jiří',
-	'kubaCode12345': 'Jakub',
-	'remziCode123': 'Remzí',
-	'risaCode123': 'Ríša',
-	'ondraCode123': 'Ondra',
-	'HelikCode123': 'Heliodor'
+	'kajacode123': 4,
+	'pajacode123': 6,
+	'strakaCode123': 2,
+	'kubaCode12345': 3,
+	'remziCode123': 4,
+	'risaCode123': 5,
+	'ondraCode123': 5,
+	'HelikCode123': 1
 }
